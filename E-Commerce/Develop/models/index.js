edce7802714c21f0440d55a4928877f967173ecc
cjsolis <cjelizondo12@gmail.com>
2023-05-07 16:20:15 -0500

    added association methods on models

--- E-Commerce/Develop/models/index.js
+++ E-Commerce/Develop/models/index.js
@@ -7,12 +7,17 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-})
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, { through: 'ProductTag'});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany( Product, { through: 'ProductTag'} )
 
 module.exports = {
   Product,
